cdae73d293670853d0ac2f78d2d725189df55816
siminabichir <simina.bichir@yahoo.com>
2023-06-18 20:57:54 +0200

    use activePage to store last visible page

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
+var activePage = "home";
+
 function hide(id) {
   document.getElementById(id).style.display = "none";
 }
 
-function hideAllPages() {
-  hide("home");
-  hide("skills");
-  hide("languages");
-  hide("projects");
-}
-
 function showPage(id) {
-  hideAllPages();
+  hide(activePage);
   var page = (document.getElementById(id).style.display = "block");
+  activePage = id;
 }
 
 showPage("home");
