990b17345c1b88657b47e943b40fd1a7d3c77f0a
siminabichir <simina.bichir@yahoo.com>
2023-06-18 15:35:12 +0200

    add showHome and showSkills functions

---
+++ index.js
@@ -0,0 +1,8 @@
+function showHome() {
+  var page = (document.getElementById("home").style.display = "block");
+}
+showHome();
+
+function showSkills() {
+  var page = (document.getElementById("skills").style.display = "block");
+}
