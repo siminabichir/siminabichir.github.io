696d979122869d394f225aecd8db4c4140e49571
siminabichir <simina.bichir@yahoo.com>
2023-06-18 16:07:59 +0200

    hide all pages before showing other pages

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
+function hideAllPages() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+}
+
 function showHome() {
+  hideAllPages();
   var page = (document.getElementById("home").style.display = "block");
 }
-showHome();
+//showHome();
 
 function showSkills() {
+  hideAllPages();
   var page = (document.getElementById("skills").style.display = "block");
 }
 
 function showLanguages() {
+  hideAllPages();
   var page = (document.getElementById("languages").style.display = "block");
 }
 
 function showProjects() {
+  hideAllPages();
   var page = (document.getElementById("projects").style.display = "block");
 }
